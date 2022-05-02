ee37de77c39b253809a2ef35662f2ce686404ddc
ecedreamer <sangit.niroula@gmail.com>
2022-05-02 22:34:34 +0545

    add bidirectional communiction feature

--- client/client.py
+++ client/client.py
@@ -1,5 +1,7 @@
-import socket            
+import socket
+import threading            
 from cryptography.fernet import Fernet
+from datetime import datetime
 
 
 HOST = '127.0.0.1'               
@@ -7,21 +9,40 @@ PORT = 5555
 KEY = 'qzC8_g45d2tW0umj7GHq6ERiJqe3aRdu6NVrhH-9xLY='
        
 
-
-
 def encrypt_msg(fernet, msg):
     return fernet.encrypt(msg.encode())
 
+def decrypt_msg(fernet, msg):
+    return fernet.decrypt(msg).decode()
+
 def close_conn(sock, fernet):
-    sock.send(encrypt_msg(fernet, "#c#c#"))
-    sock.close() 
-    print("Connection closed")
+    try:
+        sock.send(encrypt_msg(fernet, "#c#c#"))
+    except OSError:
+        print("Server connection already closed")
+    finally:
+        sock.close()
+        print("Connection closed")
+
+def receive_message(sock, fernet):
+    while True:
+        msg = sock.recv(1024)
+        decrypted_msg = decrypt_msg(fernet, msg)
+        if decrypted_msg and decrypted_msg != "#c#c#":
+            print(datetime.now().strftime("%I:%M:%S"), ">", decrypted_msg)
+        else:
+            break
+    close_conn(sock, fernet)
+
 
 def start_client(sock, fernet):
     print("type message and press enter or type q or Q to quit")
     sock.connect((HOST, PORT))
+    rm_thread = threading.Thread(target=receive_message, args=(sock, fernet))
+    rm_thread.daemon = True
+    rm_thread.start()
     while True:
-        user_input = input("> ")
+        user_input = input()
         if user_input in ["q", "Q"]:
             break
         enc_msg = encrypt_msg(fernet, user_input)
--- server/server.py
+++ server/server.py
@@ -1,5 +1,6 @@
 import socket
 from datetime import datetime
+import threading
 from cryptography.fernet import Fernet
 
 
@@ -7,15 +8,22 @@ KEY = 'qzC8_g45d2tW0umj7GHq6ERiJqe3aRdu6NVrhH-9xLY='
 HOST = "127.0.0.1"
 PORT = 5555
 
+def encrypt_msg(fernet, msg):
+    return fernet.encrypt(msg.encode())
 
 def decrypt_msg(fernet, msg):
     return fernet.decrypt(msg).decode()
 
-def close_conn(conn):
-    conn.close()
-    print("Connection closed")
+def close_conn(conn, fernet):
+    try:
+        conn.send(encrypt_msg(fernet, "#c#c#"))
+    except BrokenPipeError:
+        print("Client connection already closed")
+    finally:
+        conn.close()
+        print("Connection closed")
 
-def client_thread(conn, addr):
+def receive_message(conn, fernet):
     while True:
         msg = conn.recv(1024)
         decrypted_msg = decrypt_msg(fernet, msg)
@@ -23,29 +31,30 @@ def client_thread(conn, addr):
             print(datetime.now().strftime("%I:%M:%S"), ">", decrypted_msg)
         else:
             break
-    close_conn(conn)
+    close_conn(conn, fernet)
 
 
 def start_server(sock, fernet):
     sock.bind((HOST, PORT))
-    sock.listen(2)
+    sock.listen(1)
     conn, addr = sock.accept()
-
+    rm_thread = threading.Thread(target=receive_message, args=(conn, fernet))
+    rm_thread.daemon = True
+    rm_thread.start()
     while True:
-        msg = conn.recv(1024)
-        decrypted_msg = decrypt_msg(fernet, msg)
-        if decrypted_msg and decrypted_msg != "#c#c#":
-            print(datetime.now().strftime("%I:%M:%S"), ">", decrypted_msg)
-        else:
+        user_input = input()
+        if user_input in ["q", "Q"]:
             break
-    close_conn(conn)
-    
+        enc_msg = encrypt_msg(fernet, user_input)
+        conn.send(enc_msg)
+    close_conn(conn, fernet)
     
 if __name__ == "__main__":
     sock = socket.socket()
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     fernet = Fernet(KEY)
     try:
         start_server(sock, fernet)
     except (KeyboardInterrupt, EOFError):
-        close_conn(sock)
+        sock.close()
 
